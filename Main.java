0c15f63151e1c4e15156d633c638132128c0a251
Evergreen School <92188421+Evergree-Coded@users.noreply.github.com>
2022-05-06 22:59:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Random rand = new Random();
+
+    // Default Values
+    int milesTraveled = 0;
+    int thirst = 0;
+    int camelFatigue = 0;
+    int nativesTraveled = -20;
+    int canteen = 3;
+    boolean stillPlaying = true;
+
+    while(stillPlaying){
+      // Menu
+      System.out.println("A) Drink from your canteen");
+      System.out.println("B) Ahead at Moderate Speed");
+      System.out.println("C) Ahead at Full Speed");
+      System.out.println("D) Stop for the night");
+      System.out.println("E) Status Check");
+      System.out.println("Q) Quit");
+      System.out.println("Choose an option from above: ");
+      
+
+      if()
+      
+      
+    }
+    
+    
+    
+    
+    
+  }
+}
