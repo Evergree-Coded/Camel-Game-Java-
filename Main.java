1c6b460a3d114b53bbf3e50e3d64fc6ed4713fef
Evergreen School <92188421+Evergree-Coded@users.noreply.github.com>
2022-05-07 00:11:23 +0000

    Week 2 of Camel Game, added if statements that update default values

--- Main.java
+++ Main.java
@@ -23,9 +23,51 @@ class Main {
       System.out.println("E) Status Check");
       System.out.println("Q) Quit");
       System.out.println("Choose an option from above: ");
+      String userChoice = scan.nextLine(); // take in user input
+
+      int nativesDistance = rand.nextInt(8) + 7;
+      
+      if(userChoice.equalsIgnoreCase("a")){
+        if(canteen == 0){
+          System.out.println("You have no canteens left");
+        }
+        else{
+          canteen--;
+          thirst = 0;
+        }
+      }
+      if(userChoice.equalsIgnoreCase("b")){
+        //milesTraveled += 8*Math.random() + 5;
+        milesTraveled += rand.nextInt(8) + 5; //MIN: 5+ 0 = 0 MAX:7 + 5 = 12
+        System.out.println("You have traveled: " + milesTraveled);
+        thirst++;
+        camelFatigue++;
+        nativesTraveled += nativesDistance;
+      }
+      if(userChoice.equalsIgnoreCase("c")){
+        milesTraveled += rand.nextInt(11)+ 10;
+        System.out.println("You have traveled: " + milesTraveled);
+        thirst++;
+        camelFatigue += rand.nextInt(3) + 1; 
+        nativesTraveled += nativesDistance;
+      }
+      if(userChoice.equalsIgnoreCase("d")){
+        camelFatigue = 0;
+        System.out.println("The Camel Fatigue value: " +  camelFatigue);
+        nativesTraveled += nativesDistance;
+      }
+      if(userChoice.equalsIgnoreCase("e")){
+        System.out.println("Miles Traveled " + milesTraveled);
+        System.out.println("You have " + canteen + " canteens");
+        System.out.println("Camel Fatigue: "+ camelFatigue);
+        System.out.println("The natives are " + (milesTraveled - nativesTraveled) + " miles behind you");
+      }
+      if(userChoice.equalsIgnoreCase("q")){
+        stillPlaying = false;
+      }
       
 
-      if()
+     
       
       
     }
